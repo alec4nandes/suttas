f18ebc0bae5c85279e5071b441424d7bc64e0515
Alec <alec4nandes@gmail.com>
2023-07-15 22:38:01 -0700

    fix first line hightlight

--- public/cite.js
+++ public/cite.js
@@ -1,5 +1,6 @@
 import { notes } from "./notes.js";
 import { displaySuttaHTML } from "./display.js";
+import { removeLineNumbersAndSpacers } from "./handlers.js";
 
 async function handleCite({
     anchorNode,
@@ -16,8 +17,19 @@ async function handleCite({
         const [starts_at, ends_at] = [anchorNum, focusNum].sort(() =>
                 selectionIsBackwards() ? -1 : 1
             ),
-            note = prompt("Note:"),
-            result = { starts_at, ends_at, text, note };
+            note = prompt("Note:");
+        let lines = getLinesFromText(text);
+        const combinedOffset = anchorOffset + lines[0].length;
+        text = text.indexOf("\n")
+            ? anchorNode.nodeValue.slice(0, anchorOffset) +
+              `<span class="highlight">${anchorNode.nodeValue
+                  .slice(anchorOffset, combinedOffset)
+                  .trim()}</span>` +
+              anchorNode.nodeValue.slice(combinedOffset) +
+              text.slice(anchorNode.nodeValue.length)
+            : text;
+        lines = getLinesFromText(text);
+        const result = { starts_at, ends_at, note, lines, text };
         notes.push(result);
         await displaySuttaHTML(null, null, notes.length - 1);
         return result;
@@ -33,6 +45,13 @@ async function handleCite({
         range.detach();
         return isBackwards;
     }
+}
+
+function getLinesFromText(text) {
+    return removeLineNumbersAndSpacers(text)
+        .split("\n")
+        .map((line) => line.trim())
+        .filter(Boolean);
 }
 
 export { handleCite };
--- public/highlight.js
+++ public/highlight.js
@@ -1,34 +1,33 @@
-import { removeLineNumbersAndSpacers } from "./handlers.js";
-
 /*
     The text value of a note object contains a string
     with embedded line numbers copied from the lines table.
     If the text includes lineNumber, highlight the entire line.
 */
+
+let highlighting = false;
+
 function highlightText(lineNumber, line, note) {
     if (note) {
-        const { starts_at, ends_at, text } = note,
-            hasLine =
-                [starts_at, ends_at].includes(lineNumber) ||
-                text.includes(lineNumber);
-        if (hasLine) {
-            const isFirstLine = lineNumber === starts_at,
-                isLastLine = lineNumber === ends_at;
-            if (isFirstLine || isLastLine) {
-                const splitLines = removeLineNumbersAndSpacers(text)
-                        .split("\n")
-                        .map((line) => line.trim())
-                        .filter(Boolean),
-                    firstLine = splitLines[0],
-                    lastLine = splitLines.at(-1);
-                // first or last line highlights
-                return getHighlightHTML(
-                    line,
-                    isFirstLine ? firstLine : lastLine
-                );
+        const { starts_at, ends_at, lines, text } = note,
+            startsWithNewLine = !text.indexOf("\n"),
+            isFirstLine = lineNumber === starts_at,
+            isLastLine = lineNumber === ends_at,
+            isSpacer = line.includes("☸");
+        isFirstLine && (highlighting = true);
+        if (highlighting && !isSpacer) {
+            isLastLine && (highlighting = false);
+            if ((isFirstLine && !startsWithNewLine) || starts_at === ends_at) {
+                // first line is already formatted
+                return lines[0];
             }
-            // middle line, full highlight
-            return getHighlightHTML(line, line);
+            if (isLastLine) {
+                const lastLine = lines.at(-1);
+                return getHighlightHTML(line, lastLine);
+            }
+            if (!isFirstLine) {
+                // middle line, full highlight
+                return getHighlightHTML(line, line);
+            }
         }
     }
     return line;
@@ -39,4 +38,4 @@ function getHighlightHTML(line, text) {
     return line.replace(text, replaced);
 }
 
-export { highlightText };
+export { highlightText, getHighlightHTML };
