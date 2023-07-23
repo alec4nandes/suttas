fb44e27e69e42b3350bc5125de9624e827748237
Alec <alec4nandes@gmail.com>
2023-07-22 19:54:14 -0700

    rearrange vars

--- public/scripts/add-note-handlers.js
+++ public/scripts/add-note-handlers.js
@@ -2,6 +2,7 @@ const notes = JSON.parse(
         document.querySelector("#notes-json").textContent.trim() || "{}"
     ),
     textTable = document.querySelector("table#text"),
+    allLines = getAllLines(),
     noteButtons = document
         .querySelector("#note-buttons")
         .querySelectorAll("button");
@@ -12,9 +13,16 @@ noteButtons.forEach((button, i) => {
     button.onclick = () => getLinesHTML(notes[i]);
 });
 
+function getAllLines() {
+    return [...textTable.querySelectorAll("tr")].map((row) => {
+        const lineNum = row.querySelector(".line-number").textContent,
+            line = row.querySelector(".line").innerHTML;
+        return [lineNum, line.trim() ? line : "<br/>"];
+    });
+}
+
 function getLinesHTML(note) {
-    const { first_line, starts_at, last_line, ends_at } = note,
-        allLines = getAllLines();
+    const { first_line, starts_at, last_line, ends_at } = note;
     let highlighting = false;
     textTable.innerHTML = allLines.map(getRowHTML).join("");
     const firstHighlight = document.querySelector(".highlight");
@@ -47,12 +55,4 @@ function getLinesHTML(note) {
         (isLastLine || isSingleLine) && (highlighting = false);
         return result;
     }
-}
-
-function getAllLines() {
-    return [...textTable.querySelectorAll("tr")].map((row) => {
-        const lineNum = row.querySelector(".line-number").textContent,
-            line = row.querySelector(".line").innerHTML;
-        return [lineNum, line.trim() ? line : "<br/>"];
-    });
 }
