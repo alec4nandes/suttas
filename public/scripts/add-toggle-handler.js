5d33be900081b30979f8c7eda457e6cf72c096d3
Alec <alec4nandes@gmail.com>
2023-08-02 22:28:53 -0700

    update favicon

--- public/scripts/add-toggle-handler.js
+++ public/scripts/add-toggle-handler.js
@@ -1,15 +1,17 @@
-document.querySelector("#toggle-sutta").onclick = (e) => {
+const navElem = document.querySelector("nav"),
+    navHeight = getComputedStyle(navElem).getPropertyValue("height"),
+    toggleButton = navElem.querySelector("#toggle-sutta"),
+    readSutta = toggleButton.textContent.trim();
+
+toggleButton.onclick = (e) => {
     const { dataset } = e.target,
         { status } = dataset,
         isHidden = status === "hidden";
     document.querySelector("#sutta").style.top = isHidden
         ? 0
-        : // match px with --nav-height var
-          // in ../css/sutta.scss
-          "calc(-100% + 85px)";
+        : `calc(-100% + ${navHeight})`;
     dataset.status = isHidden ? "showing" : "hidden";
-    const readSutta = "read",
-        readBlog = "read blog post for";
+    const readBlog = "read blog post";
     e.target.textContent = e.target.textContent.replace(
         isHidden ? readSutta : readBlog,
         isHidden ? readBlog : readSutta
